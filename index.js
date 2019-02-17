9f2430d9c8d7374fe9a4e23170a62df0c3ccd35e
Collin Argo <collinargo@gmail.com>
2019-02-17 18:19:58 -0500

    change to graphql-yoga

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const db = require('./db')
+const { GraphQLServer, PubSub } = require('graphql-yoga')
 
-const express = require('express')
-// const session = require('express-session')
-const { postgraphile } = require("postgraphile")
-// const store = new (require('connect-pg-simple')(session))({ pgPromise: db })
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+  type Counter {
+    count: Int!
+    countStr: String
+  }
+  type Subscription {
+    counter: Counter!
+  }
+`
 
-const app = express()
-const port = 7000
+const resolvers = {
+  Query: {
+    hello: () => `Hello`,
+  },
+  Counter: {
+    countStr: counter => `Current count: ${counter.count}`,
+  },
+  Subscription: {
+    counter: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random().toString(36).substring(2, 15) // random channel name
+        let count = 0
+        setInterval(() => pubsub.publish(channel, { counter: { count: count++ } }), 2000)
+        return pubsub.asyncIterator(channel)
+      },
+    }
+  },
+}
 
-// app.use(session({
-//     store, 
-//     secret: 'sUpErS3cR3t',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 } // 1 days
-// }))
+const pubsub = new PubSub()
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } })
 
-app.use(postgraphile(process.env.DATABASE_URL || 'postgres:///', 'public', {
-    graphiql: true,
-}))
-
-app.listen(port, () => console.log(`pgql-test listening on port ${port}`)) 
+server.start(() => console.log(`pgql-test listening on localhost:4000`)) 
